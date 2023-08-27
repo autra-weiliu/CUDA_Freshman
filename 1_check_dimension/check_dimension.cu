cb852c08d3d1973374f0b76a245c1e61df402990
Wei Liu <wei.liu@autra.tech>
2023-08-27 22:13:32 +0800

    feat: add check dimension test

--- 1_check_dimension/check_dimension.cu
+++ 1_check_dimension/check_dimension.cu
@@ -1,20 +1,24 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
-__global__ void checkIndex(void)
-{
-  printf("threadIdx:(%d,%d,%d) blockIdx:(%d,%d,%d) blockDim:(%d,%d,%d)\
-  gridDim(%d,%d,%d)\n",threadIdx.x,threadIdx.y,threadIdx.z,
-  blockIdx.x,blockIdx.y,blockIdx.z,blockDim.x,blockDim.y,blockDim.z,
-  gridDim.x,gridDim.y,gridDim.z);
+#include <math.h>
+
+__global__ void index_check_test() {
+  // only print for first thread in each thread block  
+  if (threadIdx.x == 0 && threadIdx.y == 0) {
+    printf("%d %d\n", blockIdx.x, blockIdx.y);
+    printf("%d %d %d %d\n", blockDim.x, blockDim.y, gridDim.x, gridDim.y);
+  }
 }
+
 int main(int argc,char **argv)
 {
-  int nElem=6;
-  dim3 block(3);
-  dim3 grid((nElem+block.x-1)/block.x);
-  printf("grid.x %d grid.y %d grid.z %d\n",grid.x,grid.y,grid.z);
-  printf("block.x %d block.y %d block.z %d\n",block.x,block.y,block.z);
-  checkIndex<<<grid,block>>>();
+  size_t total_element = 1024;
+  // 16 * 16 thread block
+  dim3 block_dim(16, 16);
+  // 2 * 2 grid
+  size_t length = static_cast<size_t>(sqrt(total_element / (block_dim.x * block_dim.y)));
+  dim3 grid_dim(length, length);
+  index_check_test<<<grid_dim, block_dim>>>();
   cudaDeviceReset();
   return 0;
 }
