ede4cc9e0c33c24594f4862b31897295388e9c6f
Wei Liu <wei.liu@autra.tech>
2023-09-01 10:09:29 +0800

    chore: polish example

--- 5_thread_index/thread_index.cu
+++ 5_thread_index/thread_index.cu
@@ -1,6 +1,5 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
-#include "freshman.h"
 
 __global__ void printThreadIndex(float *A,const int nx,const int ny)
 {
@@ -8,33 +7,37 @@ __global__ void printThreadIndex(float *A,const int nx,const int ny)
   int iy=threadIdx.y+blockIdx.y*blockDim.y;
   unsigned int idx=iy*nx+ix;
   printf("thread_id(%d,%d) block_id(%d,%d) coordinate(%d,%d)"
-          "global index %2d ival %2d\n",threadIdx.x,threadIdx.y,
+          "global index %d ival %f\n",threadIdx.x,threadIdx.y,
           blockIdx.x,blockIdx.y,ix,iy,idx,A[idx]);
 }
+
 int main(int argc,char** argv)
 {
-  initDevice(0);
+  cudaSetDevice(0);
+
   int nx=8,ny=6;
   int nxy=nx*ny;
   int nBytes=nxy*sizeof(float);
 
   //Malloc
   float* A_host=(float*)malloc(nBytes);
-  initialData(A_host,nxy);
-  printMatrix(A_host,nx,ny);
+  
+  for (size_t i = 0;i < nxy;i ++) {
+    A_host[i] = (rand() % 1000) * 1.0 / 7;
+  }
 
   //cudaMalloc
   float *A_dev=NULL;
-  CHECK(cudaMalloc((void**)&A_dev,nBytes));
+  cudaMalloc((float**) &A_dev,nBytes);
 
-  cudaMemcpy(A_dev,A_host,nBytes,cudaMemcpyHostToDevice);
+  cudaMemcpy(A_dev,A_host,nBytes, cudaMemcpyKind::cudaMemcpyHostToDevice);
 
   dim3 block(4,2);
   dim3 grid((nx-1)/block.x+1,(ny-1)/block.y+1);
 
   printThreadIndex<<<grid,block>>>(A_dev,nx,ny);
 
-  CHECK(cudaDeviceSynchronize());
+  cudaDeviceSynchronize();
   cudaFree(A_dev);
   free(A_host);
 
